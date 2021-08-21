9a6ab555bac6abb6c27d080e81c634a2e7c54cb3
Soner Çelik <snrclk94@outlook.com>
2021-08-21 14:06:44 +0300

    -Car,Customer,Rental,Color,Brand data received

--- src/app/components/rental/rental.component.ts
+++ src/app/components/rental/rental.component.ts
@@ -5,20 +5,19 @@ import { RentalService } from 'src/app/services/rental/rental.service';
 @Component({
   selector: 'app-rental',
   templateUrl: './rental.component.html',
-  styleUrls: ['./rental.component.css']
+  styleUrls: ['./rental.component.css'],
 })
 export class RentalComponent implements OnInit {
-
-  rentals : Rental[] = [];
-  constructor(private rentalService:RentalService) { }
+  rentals: Rental[] = [];
+  constructor(private rentalService: RentalService) {}
 
   ngOnInit(): void {
     this.getRentals();
   }
 
-  getRentals(){
-    this.rentalService.getRentals().subscribe(response => {
+  getRentals() {
+    this.rentalService.getRentals().subscribe((response) => {
       this.rentals = response.data;
-    })
+    });
   }
 }
